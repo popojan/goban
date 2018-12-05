0fa0c32accb218ad6a22a998496a955ab6f17fdf
Jan Popelka <popojan@gmail.com>
2018-12-05 02:37:37 +0100

    windows link rocketlib statically

--- deps/make.bat
+++ deps/make.bat
@@ -1,6 +1,9 @@
+set CLEAN=
+
 REM freetype2
 if not exist freetype2 git clone http://git.sv.nongnu.org/r/freetype/freetype2.git
 cd freetype2
+if defined CLEAN rmdir /s /q build
 if not exist build mkdir build
 cd build
 cmake .. -G%STUDIO%
@@ -10,16 +13,18 @@ cd ..\..
 REM libRocket
 if not exist libRocket git clone https://github.com/libRocket/libRocket.git
 cd libRocket
+if defined CLEAN rmdir /s /q BuildDir
 if not exist BuildDir mkdir BuildDir
 cd BuildDir
-cmake -G%STUDIO% ../Build -DFREETYPE_INCLUDE_DIRS=../../freetype2/include -DFREETYPE_LIBRARY=../../freetype2/build/Release/freetype
-MSBuild.exe libRocket.sln /t:Build /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=%TARGET%
+cmake -G%STUDIO% ../Build -DBUILD_SHARED_LIBS= -DFREETYPE_INCLUDE_DIRS=../../freetype2/include -DFREETYPE_LIBRARY=../../freetype2/build/Release/freetype
+MSBuild.exe libRocket.sln /t:ReBuild /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=%TARGET%
 
 cd ..\..
 
 REM glew
 if not exist glew-with-extensions git clone https://github.com/tamaskenez/glew-with-extensions
 cd glew-with-extensions
+if defined CLEAN rmdir /s /q BuildDir
 if not exist BuildDir mkdir BuildDir
 cd BuildDir
 cmake ..\build\cmake -G%STUDIO% -UGLEW_USE_STATIC_LIBS
@@ -33,7 +38,9 @@ if not exist glm git clone https://github.com/g-truc/glm.git
 REM glyphy
 if not exist glyphy git clone https://github.com/behdad/glyphy.git
 copy _patches\goban_glyphy.vcxproj glyphy\win32
-MSBuild.exe  glyphy\win32\goban_glyphy.vcxproj /t:Build /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=%TARGET% /p:Platform=x64
+set BUILD="Build"
+if defined CLEAN set BUILD="Rebuild"
+MSBuild.exe  glyphy\win32\goban_glyphy.vcxproj /t:%BUILD% /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=%TARGET% /p:Platform=x64
 
 REM boost-process
 if not exist boost-process git clone https://github.com/BorisSchaeling/boost-process.git
--- make.bat
+++ make.bat
@@ -9,8 +9,8 @@ set MSBUILD_PATH="C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools
 set CMAKE_PATH="C:\Program Files\CMake\bin"
 set PATH=%CMAKE_PATH%;%MSBUILD_PATH%;%PATH%
 set TOOLSET=v141
-set TARGET=10.0.17134.0
-REM set TARGET=10.0.17763.0
+REM set TARGET=10.0.17134.0
+set TARGET=10.0.17763.0
 set STUDIO="Visual Studio 15 2017 Win64"
 
 set PROJECT_DIR=%~dp0
@@ -25,7 +25,6 @@ cd %PROJECT_DIR%
 mkdir build
 cd build
 cmake .. -G%STUDIO% -DCMAKE_BUILD_TYPE=Release ^
-	-DLIBROCKET_INCLUDE_DIR=../deps/libRocket/Include ^
 	-DGLEW_INCLUDE_DIR="../deps/glew-with-extensions/include" ^
 	-DGLEW_LIBRARY="../deps/glew-with-extensions/builddir/lib/Release/libglew32" ^
 	-DLIBGLYPHY_INCLUDE_DIR=../deps/glyphy/src ^
@@ -40,4 +39,4 @@ cmake .. -G%STUDIO% -DCMAKE_BUILD_TYPE=Release ^
 	-DFREETYPE_INCLUDE_DIR_ft2build=../deps/freetype2/include ^
 	-DFREETYPE_LIBRARY=../deps/freetype2/build/Release/freetype
 
-MSBuild.exe goban.sln /t:Build /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=%TARGET%
+MSBuild.exe goban.sln /t:ReBuild /p:Configuration=Release /p:PlatformToolset=%TOOLSET% /p:TargetPlatformVersion=%TARGET%
