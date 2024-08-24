ed2dc2280f1cf965d97678ec63621db5f89da3f2
Jan Popelka <popojan@protonmail.com>
2024-08-24 19:58:02 +0200

    tweaking colors, textures and light

--- data/shaders/fragment/partial/material/red.glsl
+++ data/shaders/fragment/partial/material/red.glsl
@@ -1,25 +1,27 @@
-const vec3 lpos = vec3(2.0, 12.0, 6.0);
+const vec3 lpos = vec3(2.0, 24.0, 10.0);
 const vec3 lpos2 = vec3(-4.0, 6.0, -2.0);
-const float ldia = 3.5;
+const float ldia = 2.5;
 const float ldia2 = 10.5;
 
 const float mw = 0.85;
 const float legh = 0.15;
 
 const float ambientLevel = 0.3;
-const vec4 das = vec4(1.0, ambientLevel, 1.0, 64.0);
-const vec4 das0 = vec4(1.0, 0.5*ambientLevel, 0.01, 4.0);
-const vec4 das1 = vec4(1.0, 0.2, 0.1, 4.0);
+const vec4 das = vec4(1.0, ambientLevel, 1.0, 32.0);
+const vec4 das0 = vec4(1.0, ambientLevel, 0.01, 4.0);
+const vec4 das1 = vec4(1.0, ambientLevel, 0.1, 4.0);
 
-const MAT materials[8] = MAT[8](
+const MAT materials[10] = MAT[10](
     MAT(das, mat3(vec3(0.5), vec3(0.5), vec3(0.5)), texSolidColor),
     MAT(das0, mat3(vec3(0.566,0.0196,0.1176), vec3(0.566,0.0696,0.0676), vec3(0.766,0.1196,0.1176)), texCarpet),
-    MAT(das1, mat3(vec3(0.05), vec3(0.15), vec3(0.1)), texWood),
-    MAT(das, mat3(vec3(0.92), vec3(0.95), vec3(0.99)), texWood),
-    MAT(das1, mat3(vec3(0.895,0.79,0.45), vec3(0.52,0.52,0.19), vec3(0.05)), texWood),
+    MAT(das1, mat3(vec3(0.05), vec3(0.15), vec3(0.1)), texSlate),
+    MAT(das, mat3(vec3(0.839216, 0.819608, 0.796078),vec3(0.98, 0.95,0.94), vec3(0.99)), texShell),
+    MAT(das1, mat3(vec3(0.862745, 0.717647, 0.443137), vec3(0.802745, 0.657647, 0.383137), vec3(0.0)), texWood),
     MAT(das0, mat3(vec3(0.766,0.1196,0.1176),vec3(0.966,0.1196,0.1176),vec3(0.766,0.1196,0.1176)), texCarpet),
     MAT(das0, mat3(vec3(0.566,0.0196,0.0176), vec3(0.466,0.0196,0.0176), vec3(0.666,0.0196,0.0176)), texCarpet),
-    MAT(das1, mat3(vec3(0.053333,0.0133725,0.029039), vec3(0.293333, 0.1713725, 0.038039), vec3(0.17333,0.1613725,0.089039)), texWood)
+    MAT(das1, mat3(vec3(0.053333,0.0133725,0.029039), vec3(0.293333, 0.1713725, 0.038039), vec3(0.17333,0.1613725,0.089039)), texWood),
+    MAT(das1, mat3(vec3(0.05), vec3(0.15), vec3(0.1)), texSolidColor),
+    MAT(das, mat3(vec3(0.9), vec3(0.98), vec3(0.99)), texSolidColor)
 );
 
 void initMaterials() {
@@ -31,8 +33,8 @@ void initMaterials() {
     matidx[idWhiteStone] = 3;
     matidx[idBowlBlackStone] = 2;
     matidx[idBowlWhiteStone] = 3;
-    matidx[idBlackArea] = 2;
-    matidx[idWhiteArea] = 3;
+    matidx[idBlackArea] = 8;
+    matidx[idWhiteArea] = 9;
     matidx[idCapturedBlackStone] = 2;
     matidx[idCapturedWhiteStone] = 3;
     matidx[idBoard] = 4;
